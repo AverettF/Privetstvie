611f80bfce7e287198343f8c4f1a896b7a4b7619
Fedor <71190660+AverettF@users.noreply.github.com>
2020-09-16 23:12:47 +0500

    Точно окончательная версия
    Программа работае
    Проверка работает,но без указаная ,что именно введено неверно.Зато не вызывает ошибки в методах

--- Privetstvie/Program.cs
+++ Privetstvie/Program.cs
@@ -13,44 +13,60 @@ namespace Privetstvie
     {
         static void Main(string[] args)
         {
-
-            int yearnow = DateTime.Now.Year; //2020
-            int monthnow = DateTime.Now.Month;// 09
-            int daynow = DateTime.Now.Day;//16
+            //Переменные с сегодняшней датой
+            int yearnow = DateTime.Now.Year; 
+            int monthnow = DateTime.Now.Month;
+            int daynow = DateTime.Now.Day;
 
             //Ввод переменных
             Console.WriteLine("Введите ваше имя:");
-            string name = Console.ReadLine();  //Ввод имени
+            string name = Console.ReadLine();  
 
             Console.WriteLine("Введите день нашего рождения:");
-            int day = Convert.ToInt32(Console.ReadLine()); //Ввод дня
+            int day = Convert.ToInt32(Console.ReadLine()); 
 
             Console.WriteLine("Введите месяц нашего рождения:");
-            int month = Convert.ToInt32(Console.ReadLine()); //Ввод месяца
+            int month = Convert.ToInt32(Console.ReadLine()); 
 
             Console.WriteLine("Введите год нашего рождения:");
-            int year =int.Parse(Console.ReadLine());//Ввод года
+            int year =int.Parse(Console.ReadLine());
 
-            //Проверка на коректность ввода дня месяца,а заодно и на високосность года в год рождения 
-            //(Благо метод позваляет)
-            int yearperson = 0;
-            int DayInMounth = DateTime.DaysInMonth(year, month);
+            //Проверка на корректность ввода месяца,года.Присваивание кол-вa дней в этом месяце.
+            //Если месяц или год указан неверно, то условие следующего  метода auto неверен
+            int DayInMounth = 0;
+            if ((month > 0 && month <= 12)  && (year > 0 && year <= yearnow)) 
+            { 
+                DayInMounth = DateTime.DaysInMonth(year, month);
+            }
+
+            //Проверка на коректность ввода дня,а заодно и на високосность года в год рождения 
+            //(Благо метод DayInMonth позваляет)
+            int yearperson = -1;
             if (day <= DayInMounth && day >0)
             {
-                if( day> daynow && month > monthnow)
+                
+                if( (day> daynow && month >= monthnow) || month >monthnow)
                 {
-                    yearperson = yearnow - year;
+                    yearperson = yearnow - year-1;
                 }
                 else
                 {
-                    yearperson = yearnow - year - 1;
+                    yearperson = yearnow - year ;
                 }
-
             }
-  
+            else
+            {
+                Console.WriteLine("НЕВЕРНО ВВЕДЕНА ДАТА");
+            }
 
-            Console.WriteLine("Привет, {0} .Ваш возраст равен {1} лет.Приятно познакомиться.",name,yearperson);
+            if (yearperson != -1)
+            {
+                Console.WriteLine("Привет, {0} .Ваш возраст равен {1} лет.Приятно познакомиться.", name, yearperson);
+            }
 
+
+
+         
         }
     }
 }
