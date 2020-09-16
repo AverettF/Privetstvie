56db336d94140b75d460cdae77f942a376ab446a
Fedor <71190660+AverettF@users.noreply.github.com>
2020-09-16 21:44:37 +0500

    Предварительный код.Без проверки на дурака.
    Что было сделано:
    1.Проверка дня месяца через метод DaysInMonth,а заодно и на весокосность года
    (до этого на месяц -swich,а на вискосность year%4)
    2.Теперь программа автоматически смотрит дату

--- Privetstvie/Program.cs
+++ Privetstvie/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace Privetstvie
 {
@@ -11,81 +13,43 @@ namespace Privetstvie
     {
         static void Main(string[] args)
         {
+
+            int yearnow = DateTime.Now.Year; //2020
+            int monthnow = DateTime.Now.Month;// 09
+            int daynow = DateTime.Now.Day;//16
+
             //Ввод переменных
-           
             Console.WriteLine("Введите ваше имя:");
-            string name = Console.ReadLine();
+            string name = Console.ReadLine();  //Ввод имени
 
             Console.WriteLine("Введите день нашего рождения:");
-            int day = Convert.ToInt32(Console.ReadLine());
+            int day = Convert.ToInt32(Console.ReadLine()); //Ввод дня
 
             Console.WriteLine("Введите месяц нашего рождения:");
-            int month = Convert.ToInt32(Console.ReadLine());
+            int month = Convert.ToInt32(Console.ReadLine()); //Ввод месяца
 
             Console.WriteLine("Введите год нашего рождения:");
-            int age =int.Parse(Console.ReadLine());
+            int year =int.Parse(Console.ReadLine());//Ввод года
 
-            //Проверка на високосность года
-            int vis;
-            if(age%4 == 0)
+            //Проверка на коректность ввода дня месяца,а заодно и на високосность года в год рождения 
+            //(Благо метод позваляет)
+            int yearperson = 0;
+            int DayInMounth = DateTime.DaysInMonth(year, month);
+            if (day <= DayInMounth && day >0)
             {
-                 vis = 29;
-            }
-            else
-            {
-                vis = 28;
-            }
-
-            //Здесь я немного заморочился , и сделал проверку каждого месяца
-            //Нахождение max'ого дня в месяце
-            int i = 0;
-            switch (month)
-            {
-                case 1: i = 31; break;
-                case 2: i = vis; break;
-                case 3: i = 31; break;
-                case 4: i = 30; break;
-                case 5: i = 31; break;
-                case 6: i = 30; break;
-                case 7: i = 31; break;
-                case 8: i = 31; break;
-                case 9: i = 30; break;
-                case 10: i = 31; break;
-                case 11: i = 30; break;
-                case 12: i = 31; break;
-                default: Console.WriteLine("НЕ ПРАВИЛЬНО ВВЕДЕН МЕСЯЦ"); break;
-            }
-
-            int year = 0;
-
-            //Проверка на корректность введенного дня
-            if (day <= i && day >= 1)
-            {
-                if (day > 13 && month > 9) //Нахождение сколько лет пользователю 
+                if( day> daynow && month > monthnow)
                 {
-                    year = 2020 - age - 1;//Если он родился после 13.9
+                    yearperson = yearnow - year;
                 }
                 else
                 {
-                    year = 2020 - age;//Если он родился до 13.9
+                    yearperson = yearnow - year - 1;
                 }
 
             }
-            else
-            {
-                Console.WriteLine("НЕ ПРАВИЛЬНО ВВЕДЕН ДЕНЬ!!!");
-            }
+  
+            Console.WriteLine("Привет, {0} .Ваш возраст равен {1} лет.Приятно познакомиться.",name,yearperson);
 
-            //Если хоть что-то введено с ошибкой,прог. выводит error
-            if (year == 0)
-            {
-                Console.WriteLine("error");
-            }
-            else
-            {
-                Console.WriteLine("Привет, {0}. Ваш возраст равен {1} лет. Приятно познакомиться.", name, year);
-                Console.ReadKey();
-            }
         }
     }
 }
