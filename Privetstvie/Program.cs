c802a4c017a95ffd298d87a7030162d06d6b4643
FedorShuvalov <shuvalov.fedor18@outlook.com>
2020-09-19 12:06:42 +0500

    Финальная версия
    DaysInMonth-Замена этого метода на расширенный вариант.
    Без проверки на дурака,но с проверкой на коректность ввода чисел

--- Privetstvie/Program.cs
+++ Privetstvie/Program.cs
@@ -31,18 +31,46 @@ namespace Privetstvie
             Console.WriteLine("Введите номер года даты вашего рождения:");
             int year =int.Parse(Console.ReadLine());
 
-            //Проверка на корректность ввода месяца,года.Присваивание кол-вa дней в этом месяце.
+
+            //Проверка на корректность ввода месяца,года.Присваивание кол-вa дней в месяц рождения.
             //Если месяц или год указан неверно, то условие следующего  метода auto неверен
-            int DayInMounth = 0;
-            if ((month > 0 && month <= 12)  && (year > 0 && year <= yearnow)) 
-            { 
-                DayInMounth = DateTime.DaysInMonth(year, month);
+            int DayInMonth = 0;
+            int Feb = 0;
+            if ((month > 0 && month <= 12) && (year > 0 && year <= yearnow))
+            {
+                //Проверка на високосность года
+                if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)   //  DayInMounth = DateTime.DaysInMonth(year, month);
+                {
+                    Feb = 29;
+                }
+                else
+                {
+                    Feb = 28;
+                }
+
+                switch (month)
+                {
+                    case 1: DayInMonth = 31; break;
+                    case 2: DayInMonth = Feb; break;
+                    case 3: DayInMonth = 31; break;
+                    case 4: DayInMonth = 30; break;
+                    case 5: DayInMonth = 31; break;
+                    case 6: DayInMonth = 30; break;
+                    case 7: DayInMonth = 31; break;
+                    case 8: DayInMonth = 31; break;
+                    case 9: DayInMonth = 30; break;
+                    case 10: DayInMonth = 31; break;
+                    case 11: DayInMonth = 30; break;
+                    case 12: DayInMonth = 31; break;
+                    default: Console.WriteLine("НЕ ПРАВИЛЬНО ВВЕДЕН МЕСЯЦ"); break;
+                }
+
             }
 
-            //Проверка на коректность ввода дня,а заодно и на високосность года в год рождения 
-            //(Благо метод DayInMonth позваляет)
+
+            //Проверка на коректность ввода дня и был ли день рождения в этом году 
             int yearperson = -1;
-            if (day <= DayInMounth && day >0)
+            if (day <= DayInMonth && day >0)
             {
                 
                 if( (day> daynow && month >= monthnow) || month >monthnow)
@@ -64,9 +92,6 @@ namespace Privetstvie
                 Console.WriteLine("Привет, {0} .Ваш возраст равен {1} лет.Приятно познакомиться.", name, yearperson);
             }
 
-
-
-         
         }
     }
 }
